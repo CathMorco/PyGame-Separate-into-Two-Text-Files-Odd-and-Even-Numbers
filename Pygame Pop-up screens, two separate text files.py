986e8117f69248329c8c8d7729a6a674289a2b79
unknown <morco.cathleenjan.12192003@gmail.com>
2023-04-23 00:49:40 +0800

    Pseudocode

---
+++ Pygame Pop-up screens, two separate text files.py
@@ -0,0 +1,57 @@
+#Imports the necessary modules
+import pygame
+
+#Sets the value for the width and height of the display screen
+
+
+#Creates the display screen
+
+
+#RGB example values
+
+
+#The rate of change in colors
+
+
+#The color directory & its values
+
+
+#Opens and reads each individual line in the text file
+
+
+#Divides the list into even and odd numbers
+
+
+#Tranfers the even and odd list of numbers into 2 separate text files
+
+
+
+#Creates the main program
+
+
+    #Sets the title for the screen display
+    
+
+    #Multiplies the color directory and its values in accordance to the number of items in its list
+    
+
+    #Initialized values for functions
+    
+    #Draws the text
+    
+
+    #Creates the color change
+    
+
+    #Combines the color change and draw text into one function
+    
+    # Initialising pygame
+    
+
+    #Creates the properties of the background screen so it can scroll
+    
+
+    #Runs the program
+    
+
+#Runs the program for the 2 text files
