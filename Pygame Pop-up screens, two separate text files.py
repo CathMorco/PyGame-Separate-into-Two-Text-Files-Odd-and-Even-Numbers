0d6bcbc055fc29300fbf241de9a0859f8ed98b38
unknown <morco.cathleenjan.12192003@gmail.com>
2023-04-23 00:51:30 +0800

    Create display screen

--- Pygame Pop-up screens, two separate text files.py
+++ Pygame Pop-up screens, two separate text files.py
@@ -5,7 +5,9 @@ import pygame
 W, H = 800, 600
 
 #Creates the display screen
-
+display = pygame.Surface ((W, H))
+screen = pygame.display.set_mode ((W, H))
+clock = pygame.time.Clock()
 
 #RGB example values
 
